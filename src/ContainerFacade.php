ab96283050af78363222f7e5019315b66aa6b8a4
sinpe <18222544@qq.com>
2018-12-26 09:18:27 +0800

    fix

--- src/ContainerFacade.php
+++ src/ContainerFacade.php
@@ -8,7 +8,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Sinpe\Container\Facades;
+namespace Sinpe\Container;
 
 use Psr\Container\ContainerInterface as PsrContainerInterface;
 
